b6466c84a4d2c8110182a72fe24b300e0be624c4
Brent Jackson <jxnblk@gmail.com>
2017-07-25 22:39:30 -0400

    Edit features section

--- docs/Features.js
+++ docs/Features.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import Section from './Section'
+import {
+  Text
+} from './ui'
 
 const Features = () => (
   <Section name='Features'>
-    boop
+    {features.map(feat => (
+      <Text key={feat}>
+        {feat}
+      </Text>
+    ))}
   </Section>
 )
 
 const features = [
-  'Under 1kb gzipped',
-  '52 LOC',
-  'Fast',
-  'Simple API',
-  'Use JavaScript objects for styling',
-  'Pseudoclasses',
-  'Media queries',
-  'Child selectors',
-  'Atomic code deduplication',
+  '- Under 1kb gzipped',
+  '- 52 lines of code',
+  '- Fast',
+  '- Simple API',
+  '- Use JavaScript objects for styling',
+  '- Pseudoclasses',
+  '- Media queries',
+  '- Atomic code deduplication',
 ]
 
 export default Features
